7d321d3d030409e37b9c22bc7ac78d85bd48f360
JOSE CARLOS CARBONE <MT27789605@osdebinario.grupoosde>
2017-08-17 20:19:18 -0300

    initial commit

---
+++ eclipsedocker/docker-eclipse/run.ps1
@@ -0,0 +1,1 @@
+docker run -ti --rm --name=eclipse -u 1000:1000 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/Projects:/Projects -v $(pwd)/Projects/Eclipse/Workspace/mars:/workspace  nsa/eclipse:mars
