f7c0589eb624215fecbb1f3902c0fed6e4f99079
carbonecar <carbonecar@gmail.com>
2017-09-02 13:13:56 -0300

    quick fix for mac

--- docker-eclipse/run-eclipse.sh
+++ docker-eclipse/run-eclipse.sh
@@ -2,7 +2,7 @@ docker run -ti \
            --rm \
            --name=eclipse \
            -u 1000:1000 \
-           -e DISPLAY=$DISPLAY \
+           -e DISPLAY=192.168.0.3:0 \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
            -v ~/Projects:/Projects \
            -v ~/Projects/Eclipse/Workspace/mars:/workspace \
